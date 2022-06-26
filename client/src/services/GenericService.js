1b799c0018000e3440907cd72037d0749005db5e
afaq-khan2000 <kafaq885@gmail.com>
2022-06-27 02:29:10 +0500

    Update GenericService.js

--- client/src/services/GenericService.js
+++ client/src/services/GenericService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.baseURL = "http://localhost:4000/";
-// axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
+axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
   constructor() { }
   get = (url) =>
